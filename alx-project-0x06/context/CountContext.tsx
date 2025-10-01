6760e41c30e5a1af94ed4653e32bedc92d97d98b
codebyemma <mysubbacc@gmail.com>
2025-10-01 23:20:31 +0100

    df

--- alx-project-0x06/context/CountContext.tsx
+++ alx-project-0x06/context/CountContext.tsx
@@ -2,10 +2,10 @@ import { createContext, useContext,  useState, ReactNode } from "react"
 
 interface CountContextProps {
   count: number
-  increment: () => void
-  decrement: () => void
+  increment: () => void;
+  decrement: () => void;
 }
-
+["increment: () => void;", "decrement: () => void;"]
 export const CountContext = createContext<CountContextProps | undefined>(undefined)
 
 export const CountProvider = ({ children }: { children: ReactNode}) => {
